f492db49ee36202c709b8ab5ff62c14f68248ec0
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-02-03 08:39:12 -0600

    Floating banner added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,44 @@
 'use strict';
+'use strict';
+/*----------------------------------------------------------->
+	Utility Functions
+<------------------------------------------------------------*/
+
+function select(selector, scope = document) {
+  return scope.querySelector(selector);
+}
+function selectAll(selector, scope = document) {
+  return scope.querySelectorAll(selector);
+}
+function listen(event, element, callback) {
+  return element.addEventListener(event, callback);
+}
+
+/*----------------------------------------------------------->
+	Selectors 
+<------------------------------------------------------------*/
+
+const navBar = select(".nav-wrapper");
+const heroBanner = select(".hero-banner");
+const headerSwitch = heroBanner.offsetHeight;
+const linkWrapper = select('.link-wrapper');
+const navTitle = select('h3');
+
+/*----------------------------------------------------------->
+	Listeners 
+<-----------------------------------------------------------*/
+
+listen("scroll", window, () => {
+	const trigger = window.scrollY;
+
+	if (trigger > headerSwitch) {
+		navBar.classList.add("visible");
+	} else {
+		navBar.classList.remove("visible");
+	}
+});
+
+listen('click', burgerMenu, () => {
+	linkWrapper.classList.toggle("visible");
+  navTitle.classList.toggle("visible");
+});
